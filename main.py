76d97276a245a203c64411d9a641b2d96a4d41de
Hobin Kwak <76905712+hobinkwak@users.noreply.github.com>
2024-06-21 22:11:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,53 +3,6 @@ from riskpremia import *
 if __name__ == '__main__':
     from pandas_datareader import data as web
 
-    ######### FRED-MD PCA Factor ######
-    #################################
-    macro = pd.read_csv('D:/workspace/project/Macro Factor Mimicking Portfolio/fmp/data/factors.csv',
-                        index_col=['date'], parse_dates=['date'])
-    port = pd.read_csv('D:/workspace/project/Risk premia Estimation/data/port.csv', index_col=[0],
-                       parse_dates=[0]).dropna()
-    port /= 100
-    start, end = max(port.index[0], macro.index[0]), min(port.index[-1], macro.index[-1])
-    macro = macro.loc[start:end]
-    port = port.loc[start:end]
-
-    ff_factor = 'F-F_Research_Data_5_Factors_2x3'
-    ff_factor_data = web.DataReader(ff_factor, 'famafrench', start=start, end=end)[0]
-    ff_factor_data /= 100
-
-    port = port - ff_factor_data['RF'].values.reshape(-1, 1)
-    Est = Estimator(port, macro)
-
-    res_two = Est.two_pass(True, lag=1).T
-    res_three = Est.three_pass(max_k=300, lag=1).T
-    print(res_two)
-    print(res_three)
-
-    ######### FRED-MD Sparse PCA Factor ######
-    #################################
-    macro = pd.read_csv('D:/workspace/project/MacroPCA/result/factors sparse.csv',
-                        index_col=['date'], parse_dates=['date'])
-    port = pd.read_csv('D:/workspace/project/Risk premia Estimation/data/port.csv', index_col=[0],
-                       parse_dates=[0]).dropna()
-    port /= 100
-    start, end = max(port.index[0], macro.index[0]), min(port.index[-1], macro.index[-1])
-    macro = macro.loc[start:end]
-    port = port.loc[start:end]
-
-    ff_factor = 'F-F_Research_Data_5_Factors_2x3'
-    ff_factor_data = web.DataReader(ff_factor, 'famafrench', start=start, end=end)[0]
-    ff_factor_data /= 100
-
-    port = port - ff_factor_data['RF'].values.reshape(-1, 1)
-    Est = Estimator(port, macro)
-
-    res_two = Est.two_pass(True, lag=None).T
-    res_three = Est.three_pass(max_k=300, lag=None).T
-    print(res_two)
-    print(res_three)
-
-
     ###################
     ########### ff5 factor #############
     #################
@@ -74,44 +27,17 @@ if __name__ == '__main__':
     print(res_two)
     print(res_three)
 
-    ###################
-    ##### FRED-MD group별 First PCA #####
-    ###########################
-    group_info = {1: 'Output and income',
-                  2: 'Labor market',
-                  3: 'Housing',
-                  4: 'Consumption, orders, and inventories',
-                  5: 'Money and credit',
-                  6: 'Interest and exchange rates',
-                  7: 'Prices',
-                  8: 'Stock market'}
-    group_factors = {}
-    for g in group_info.values():
-        group_factors[g] = \
-            pd.read_csv(f'D:/workspace/project/MacroPCA/result/factors {g}.csv', index_col=['date'],
-                        parse_dates=['date'])[
-                '0']
-    macro = pd.DataFrame(group_factors)
-    port = pd.read_csv('D:/workspace/project/Risk premia Estimation/data/port.csv', index_col=[0],
-                       parse_dates=[0]).dropna()
-    port /= 100
-    start, end = max(port.index[0], macro.index[0]), min(port.index[-1], macro.index[-1])
-    macro = macro.loc[start:end]
-    port = port.loc[start:end]
-    ff_factor = 'F-F_Research_Data_5_Factors_2x3'
-    ff_factor_data = web.DataReader(ff_factor, 'famafrench', start=port.index[0], end=port.index[-1])[0]
-    ff_factor_data /= 100
-    port = port - ff_factor_data['RF'].values.reshape(-1, 1)
-
-    Est = Estimator(port, macro)
-    res_two = Est.two_pass(True, lag=None).T
-    res_three = Est.three_pass(max_k=300, lag=None).T
-    print(res_two)
-    print(res_three)
-
     ################
     ##### Macro Factor ####
     #############
+
+
+    """
+    The macro factors were constructed based on the methodology(=Mimicking Portfolios)  of 
+    Macro Factor-Mimicking Portfolios (2022).
+    """
+
+    
 
     macro = pd.read_csv('D:/workspace/project/Risk premia Estimation/data/macro.csv',
                         index_col=[0], parse_dates=[0])[
